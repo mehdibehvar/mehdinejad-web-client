a2b5c0b5dcf49d4dd0caedc6406f720b4e2805c2
mehdi behvar <97731800+mehdibehvar@users.noreply.github.com>
2024-01-01 16:36:32 +0330

    config-vite- to-support-multiple-pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import path from 'node:path';
-
+import { resolve } from 'path';
 import { partytownVite } from '@builder.io/partytown/utils';
 import legacy from '@vitejs/plugin-legacy';
 
@@ -9,5 +9,21 @@ export default {
     partytownVite({
       dest: path.join(__dirname, 'dist', '~partytown')
     })
-  ]
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'views/about.html'),
+        blog: resolve(__dirname, 'views/blog.html'),
+        blogs: resolve(__dirname, 'views/blogs.html'),
+        contactus: resolve(__dirname, 'views/contactus.html'),
+        course: resolve(__dirname, 'views/course.html'),
+        courses: resolve(__dirname, 'views/courses.html'),
+        padcast: resolve(__dirname, 'views/padcast.html'),
+        padcasts: resolve(__dirname, 'views/padcasts.html'),
+        video: resolve(__dirname, 'views/video.html'),
+      },
+    },
+  },
 };
