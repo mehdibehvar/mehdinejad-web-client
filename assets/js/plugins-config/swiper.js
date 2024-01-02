9ed28baeefa11076a59873bd08b28c97dd9da4f7
mehdibehvar <mehdibehvar00@gmail.com>
2024-01-02 23:12:13 +0330

    fix-slider-overflow

--- assets/js/plugins-config/swiper.js
+++ assets/js/plugins-config/swiper.js
@@ -61,7 +61,7 @@ const articlesSwiper = new Swiper('.articles-swiper', {
 
   direction: 'horizontal',
   loop: false,
-  slidesPerView: 1.3,
+  slidesPerView: 1.2,
   effect: "cards",
   spaceBetween: 22,
   breakpoints: {
@@ -88,7 +88,7 @@ const coursesSwiper = new Swiper('.courses-swiper', {
   modules: [Navigation],
   direction: 'horizontal',
   loop: false,
-  slidesPerView: 1.3,
+  slidesPerView: 1.2,
   effect: "cards",
   spaceBetween: 22,
   breakpoints: {
